94f10bf20400a4acd751492a5d1276ea244f63cd
y.k <yukke5222@gmail.com>
2024-03-04 22:23:16 +0900

    test: ✅ リリースノートにdescriptionを追加しモデルスペックを追記 #18

--- app/models/release_note.rb
+++ app/models/release_note.rb
@@ -2,15 +2,32 @@
 #
 # Table name: release_notes
 #
-#  id         :uuid             not null, primary key
-#  version    :string(191)      not null
-#  title      :string(191)      not null
-#  created_at :datetime
-#  updated_at :datetime
+#  id          :uuid             not null, primary key
+#  description :text             not null
+#  title       :string(191)      not null
+#  version     :string(191)      not null
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+# Indexes
+#
+#  index_release_notes_on_version  (version) UNIQUE
 #
 class ReleaseNote < ApplicationRecord
   with_options presence: true do
     validates :version
     validates :title
+    validates :description
+  end
+
+  validates :version,
+            uniqueness: { case_sensitive: false },
+            format: {
+              with: /\A\d+\.\d+\.\d+\z/,
+              message: 'はセマンティックバージョニングに従ってください'
+            }
+
+  def self.latest
+    order(version: :desc).first
   end
 end
