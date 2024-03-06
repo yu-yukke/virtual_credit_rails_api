9889dfa493113b78d59c3fd03f44489b185002c4
y.k <yukke5222@gmail.com>
2024-03-07 00:02:24 +0900

    docs: 📝 ReleaseNoteSerializerへannotate書き出し

--- app/serializers/api/v1/release_note_serializer.rb
+++ app/serializers/api/v1/release_note_serializer.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: release_notes
+#
+#  id          :uuid             not null, primary key
+#  description :text             not null
+#  title       :string(191)      not null
+#  version     :string(191)      not null
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+# Indexes
+#
+#  index_release_notes_on_version  (version) UNIQUE
+#
 class Api::V1::ReleaseNoteSerializer < ActiveModel::Serializer
   attributes :id, :version, :title, :description, :created_at
 end
