8127f7ea2d60ed822a1d1e98960b21661661442f
y.k <yukke5222@gmail.com>
2024-03-12 00:38:01 +0900

    feat: ✨ devise標準の登録コントローラーを独自コントローラーへ向けるルーティングを定義 #21

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      mount_devise_token_auth_for 'User', at: 'auth'
+      mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks], controllers: {
+        registrations: 'api/v1/auth/registrations'
+      }
 
       resources :release_notes, only: %i[index]
     end
