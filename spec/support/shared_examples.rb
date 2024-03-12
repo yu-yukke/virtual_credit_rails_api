6e508858b1a0b852aa072e8637f485a891b881f9
y.k <yukke5222@gmail.com>
2024-03-12 12:14:16 +0900

    test: ✅ ユーザー登録specを定義 #21

--- spec/support/shared_examples.rb
+++ spec/support/shared_examples.rb
@@ -8,6 +8,16 @@ shared_examples 'ok' do
   end
 end
 
+shared_examples 'created' do
+  it 'returns created' do
+    expect(response).to have_http_status(:created)
+  end
+
+  it 'returns 201 response schema' do
+    assert_response_schema_confirm(201)
+  end
+end
+
 shared_examples 'bad request' do
   it 'returns bad request' do
     expect(response).to have_http_status(:bad_request)
@@ -15,5 +25,25 @@ shared_examples 'bad request' do
 
   it 'returns 400 response schema' do
     assert_response_schema_confirm(400)
+  end
+end
+
+shared_examples 'conflict' do
+  it 'returns conflict' do
+    expect(response).to have_http_status(:conflict)
+  end
+
+  it 'returns 409 response schema' do
+    assert_response_schema_confirm(409)
+  end
+end
+
+shared_examples 'unprocessable entity' do
+  it 'returns unprocessable entity' do
+    expect(response).to have_http_status(:unprocessable_entity)
+  end
+
+  it 'returns 422 response schema' do
+    assert_response_schema_confirm(422)
   end
 end
