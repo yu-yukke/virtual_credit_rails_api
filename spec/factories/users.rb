3ccb854df8db4e51ad21baaea3d36c6ae7a64afe
y.k <yukke5222@gmail.com>
2024-03-12 11:24:43 +0900

    test: ✅ UserFactoryのパスワードの長さを修正 #21

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -36,7 +36,7 @@ FactoryBot.define do
   factory :user do
     name { Faker::Internet.username }
     email { Faker::Internet.email }
-    password = Faker::Internet.password(min_length: 6)
+    password = Faker::Internet.password(min_length: 8)
     password { password }
     password_confirmation { password }
   end
