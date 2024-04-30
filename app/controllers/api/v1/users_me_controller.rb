d7e32e6ee592b8fb54659e4be0967f4c38661c33
y.k <yukke5222@gmail.com>
2024-04-30 20:29:17 +0900

    mod: 🔧 ユーザー更新時に全てのパラメータを必須ではなくす #50

--- app/controllers/api/v1/users_me_controller.rb
+++ app/controllers/api/v1/users_me_controller.rb
@@ -1,8 +1,6 @@
 module Api
   module V1
     class UsersMeController < ApplicationController
-      before_action :check_update_params, only: :update
-
       def show
         data = ActiveModelSerializers::SerializableResource.new(
           current_api_v1_user,
@@ -36,15 +34,7 @@ module Api
       private
 
       def update_params
-        params.permit(:name, :slug, :description, :image)
-      end
-
-      def check_update_params
-        check_required_params(
-          resource: 'User',
-          required_params: User::UPDATE_PARAMS,
-          requested_params: update_params
-        )
+        params.permit(:name, :slug, :description, :thumbnail_image)
       end
     end
   end
