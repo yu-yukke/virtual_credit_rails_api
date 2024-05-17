b33d99eb63c35e50f126df71b89f26dfa436c662
y.k <yukke5222@gmail.com>
2024-05-17 23:06:39 +0900

    feat: ✨ test環境でbulletを有効化 #53

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -8,6 +8,12 @@ require "active_support/core_ext/integer/time"
 Rails.application.routes.default_url_options[:host] = 'localhost'
 Rails.application.routes.default_url_options[:port] = 4000
 Rails.application.configure do
+  config.after_initialize do
+    Bullet.enable        = true
+    Bullet.raise         = true # raise an error if n+1 query occurs
+    Bullet.stacktrace_excludes = ["_spec.rb"]
+  end
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Turn false under Spring and add config.action_view.cache_template_loading = true.
