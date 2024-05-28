61038b4c6897f21f6c06207559e4fe356c49281b
y.k <yukke5222@gmail.com>
2024-05-29 00:13:44 +0900

    feat: ✨ スキル作成APIの実装 #65

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -13,6 +13,8 @@
 #  index_skills_on_name  (name) UNIQUE
 #
 class Skill < ApplicationRecord
+  CREATE_PARAMS = %w[name].freeze
+
   belongs_to :created_user, class_name: 'User', foreign_key: 'created_by',
                             inverse_of: :created_skills, optional: true
 
