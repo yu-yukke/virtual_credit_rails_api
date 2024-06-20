acb55b5284982e5fc6c21d44299585fdac230e4e
y.k <yukke5222@gmail.com>
2024-06-20 21:10:21 +0900

    feat: ✨ 作品カテゴリーテーブルの作成 #79

---
+++ app/models/category.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: categories
+#
+#  id         :uuid             not null, primary key
+#  created_by :uuid
+#  name       :string           not null
+#  created_at :datetime
+#  updated_at :datetime
+#
+# Indexes
+#
+#  index_categories_on_name  (name) UNIQUE
+#
+class Category < ApplicationRecord
+  belongs_to :created_user, class_name: 'User', foreign_key: 'created_by',
+                            inverse_of: :created_categories, optional: true
+
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+end
