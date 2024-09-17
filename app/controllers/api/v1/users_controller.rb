fc2de02790b68a4f98f6c6ffe465961743e3f655
y.k <yukke5222@gmail.com>
2024-09-18 03:11:38 +0900

    feat: ✨ ユーザー一覧返却APIを実装 #135

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,26 @@
+module Api
+  module V1
+    class UsersController < ApplicationController
+      skip_before_action :authenticate_api_v1_user!, only: %i[index]
+
+      before_action :check_page_params, only: %i[index]
+
+      def index
+        users = User.published.page(params[:page])
+
+        data = ActiveModelSerializers::SerializableResource.new(
+          users,
+          each_serializer: Api::V1::UserWithWorksSerializer
+        ).serializable_hash
+
+        render(
+          json: {
+            data:,
+            meta: pagination(users)
+          },
+          status: :ok
+        )
+      end
+    end
+  end
+end
